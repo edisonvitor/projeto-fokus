4113eaa04ec9cdf75305bf1fda494e0383b21b83
Edison Vitor <edison.vitor@hotmail.com>
2024-02-13 23:03:45 -0300

    resolvendo img

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active');
     })
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
         case 'foco':
             titulo.innerHTML = `Otimize sua produtividade,<br>
@@ -101,11 +101,11 @@ function iniciarOuPausar() {
     somInciar.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
     textBtIniciarPausar.textContent = 'Pausar';
-    imgBtIniciarPausar.setAttribute('src', '/imagens/pause.png');
+    imgBtIniciarPausar.setAttribute('src', './imagens/pause.png');
 }
 function zerar() {
     clearInterval(intervaloId);
-    imgBtIniciarPausar.setAttribute('src', '/imagens/play_arrow.png');
+    imgBtIniciarPausar.setAttribute('src', './imagens/play_arrow.png');
     intervaloId = null;
     tempoEmSegundos = 5
 }
